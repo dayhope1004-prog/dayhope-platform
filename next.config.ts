c4aee44e208b931ac0e661ecc317d47485ca9c1d
dayhope1004-prog <dayhope1004@gmail.com>
2026-02-20 23:05:42 +0900

    Add export config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 이게 있어야 배포가 돼! (중요)
+  images: {
+    unoptimized: true, // 👈 Cloudflare 무료 버전 필수 설정
+  },
 };
 
 export default nextConfig;
